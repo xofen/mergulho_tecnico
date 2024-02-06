6879cc4e8b4c504678cbfd8445f7e1ad97b9ddcc
Xofen <pedromirilli@gmail.com>
2024-02-06 19:06:13 -0300

    caminhando e cantando

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function myFunction() {
   var x = document.getElementById("menu");
+  var y = document.getElementById("burger");
   if (x.style.display === "block") {
     x.style.display = "none";
+    y.innerHTML = '<i class="fa-solid fa-bars"></i>';
   } else {
     x.style.display = "block";
     x.style.animation = "droplow 1.3s";
     x.style.borderBottom = "4px solid gray";
+    y.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   }
 }
